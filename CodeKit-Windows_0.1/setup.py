5421cd88ce347d47f44c7a2fcf9526bfda83b50a
Sidecans <sidbhave@hotmail.com>
2023-01-06 12:25:18 -0800

    Made Base 2

--- CodeKit-Windows_0.1/setup.py
+++ CodeKit-Windows_0.1/setup.py
@@ -1,22 +1,27 @@
 import os 
 import time
 import sys
-from getkey import getkey, keys
+import webbrowser
 print("Welcome to the setup process of EasyAI")
 time.sleep(0.6)
-os.system('cls')
-print("Setup Program [1]")
-print("Credits[2]")
-print("Exit [3]")
-key = getkey()
-if key == "1":
-    pass
-
-if key == "2":
-    print("Client Dev: Sidecan")
-    print("Front-End Dev: Cactus__ & hernan do")
-
-if key == "3":
-    sys.exit()
+def gui():
+    os.system('cls')
+    print("Setup Program [1]")
+    print("Credits[2]")
+    print("Exit [3]")
+    key = input()
+    if key == "1":
+        os.system('cls')
+        webbrowser.open_new()
+    elif key == "2":
+        print("Client Dev: Sidecan")
+        print("Front-End Dev: Cactus__ & hernan do")
+        time.sleep(0.6)
+        gui()
+    elif key == "3":
+        sys.exit()
+    else:
+        gui()
+gui()
 
 
