ab56c96ba042bfd9f3f7cab9fe00852fd7ecc9c6
Sidecans <sidbhave@hotmail.com>
2023-01-06 17:00:20 -0800

    Made Step 0.8

---
+++ scrap_windows/setup.py
@@ -0,0 +1,28 @@
+import os 
+import time
+import sys
+import webbrowser
+print("Welcome to the setup process of EasyAI")
+time.sleep(0.6)
+def gui():
+    os.system('cls')
+    print("Setup Program [1]")
+    print("Credits[2]")
+    print("Exit [3]")
+    key = input()
+    if key == "1":
+        os.system('cls')
+        print("Setup Finished")
+        time.sleep(1)
+    elif key == "2":
+        print("Client Dev: Sidecan")
+        print("Front-End Dev: Cactus__ & hernan do")
+        time.sleep(0.6)
+        gui()
+    elif key == "3":
+        sys.exit()
+    else:
+        gui()
+gui()
+
+
