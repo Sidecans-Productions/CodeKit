27b1b21ba2bfb04b00ed0399e0f656fa2247acc5
Sidecans <sidbhave@hotmail.com>
2023-01-14 09:23:39 -0800

    Made Base Aplha |1.0

--- CodeKit-Windows_0.1/src/interpreter.py
+++ CodeKit-Windows_0.1/src/interpreter.py
@@ -1,17 +1,25 @@
 import os
 def interpret(file):
-    languages = {"bas":"BASIC"}
-    languagesstr = ("BASIC")
-    extension = file.split(".")[1]
+    languages = {"bas": "BASIC", "c": "C"}
+    extensionsa = file.split(".")
+    extension = extensionsa[2]
+    print(extensionsa)
     os.system('cls')
     print("File Selected: " + file)
-    print("Detected Language: " + languages[extension])
+    print("Detected Language: " + str(languages[extension]))
     print("Continue[1]")
     #print("Manually Select[2]")
     key = input()
     if key == "1":
         os.system('cls')
         selected = languages[extension]
+        if selected == "C":
+            with open(file, "r+") as f:
+                contents = f.readlines()
+                x = len(f.readlines())
+                x+= 1
+
+
 
 
         
